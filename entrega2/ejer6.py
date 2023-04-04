adacbff0131f305be8db3ac84e12569d59cbd760
Franco Garcia <garciacfranco2001@gmail.com>
2023-04-04 18:39:29 -0300

    ejercicio 6 terminado

---
+++ entrega2/ejer6.py
@@ -0,0 +1,8 @@
+#6) Retomamos el código visto en la teoría, que informaba si una palabra contenía la letra a en una palabra ingresada,
+#modificarlo para saber si una palabra contiene la letra 'a' y 'n'.
+
+palabra = input("Ingresá una palabra: ")
+if "a" and "n" in palabra:
+    print("Hay letras a y n.")
+else:
+    print("No hay letras a ni n. ")
